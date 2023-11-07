e7bc95962b090b6bd5289efd3a5f9a4fda27d106
unknown <daniel.heink@fau.de>
2023-11-07 15:48:22 +0100

    Add and Fill setup.py, Fill init

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='mathquiz',
+    version='1.0.0',    
+    description='A fun math quiz',
+
+    install_requires=['random', 'unittest']
+                      ],
+
+    classifiers=[
+       
+        'Intended Audience :: DSSS Team',
+        
+    ],
+)
